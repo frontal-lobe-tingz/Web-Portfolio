cbfc44dc26ae0997afafc11fa23213096a4965eb
frontal lobe tingz <ndumisotheodore@gmail.com>
2025-05-10 22:36:32 +0200

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -18,7 +18,7 @@ const Footer = async () => {
     return (
         <footer className="text-center pb-5" id="contact">
             <div className="container">
-                <p className="text-lg">Have a project in mind?</p>
+                <p className="text-lg">Learning ,Growing and, Upskilling with every project built </p>
                 <a
                     href={`mailto:${GENERAL_INFO.email}`}
                     className="text-3xl sm:text-4xl font-anton inline-block mt-5 mb-10 hover:underline"
